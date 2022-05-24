f34aea44101883bdea9db28a2839beb85ed00e4a
DanielRygaard <72791196+Danryg@users.noreply.github.com>
2022-05-24 22:16:17 +0200

    Started adding background box

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { Box } from "@mui/material";
 
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <Box maxWidth="100vw" maxHeight="100vh" overflow="hidden">
+      <Box
+        bgcolor="#F5F0DC"
+        sx={{
+          transform: "rotate(-40deg)",
+          height: "1101.5681289728393px",
+          width: "2095.0881608950067px",
+        }}
+      />
+    </Box>
+  );
 }
