e8c1624e68372127ec74e53056c7691204fbeb64
DanielRygaard <72791196+Danryg@users.noreply.github.com>
2022-05-25 08:30:37 +0200

    Added the necessary components

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,17 +1,57 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, Button, Grid, Stack, Typography } from "@mui/material";
+import ProfileCard from "../components/ProfileCard";
 
 export default function Home() {
   return (
-    <Box maxWidth="100vw" maxHeight="100vh" overflow="hidden">
+    <>
       <Box
-        bgcolor="#F5F0DC"
-        sx={{
-          transform: "rotate(-40deg)",
-          height: "1101.5681289728393px",
-          width: "2095.0881608950067px",
-        }}
-      />
-    </Box>
+        maxWidth="100vw"
+        maxHeight="100vh"
+        overflow="hidden"
+        position="absolute"
+      >
+        <Box
+          bgcolor="#F5F0DC"
+          position="relative"
+          sx={{
+            transform: "rotate(-40deg)",
+            height: "1501.5681289728393px",
+            width: "2095px",
+            left: "-500px",
+            top: "-900px",
+          }}
+        />
+      </Box>
+      <Stack
+        width="100vw"
+        height="100vh"
+        direction="row"
+        justifyContent="space-evenly"
+        alignItems="center"
+        position="absolute"
+      >
+        <Box>
+          <ProfileCard />
+        </Box>
+
+        <Stack>
+          <Stack>
+            <Typography fontSize="90px" fontWeight="bold">
+              Hello
+            </Typography>
+            <Typography fontSize="60px" fontWeight="bold" textAlign="center">
+              This is me
+            </Typography>
+          </Stack>
+          <Grid container columns={2} wrap="wrap">
+            <Button variant="outlined">Projects</Button>
+            <Button variant="outlined">CV</Button>
+            <Button variant="outlined">WHO AM I</Button>
+            <Button variant="outlined">EXPERIENCE</Button>
+          </Grid>
+        </Stack>
+      </Stack>
+    </>
   );
 }
