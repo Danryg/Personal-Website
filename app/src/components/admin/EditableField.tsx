f8798726abe79f3298007b3d5bd78c5edf5dad42
Daniel Rygaard <daniel.rygaard@weknowit.se>
2022-10-01 22:45:17 +0200

    Created 2 components for editing fields

---
+++ app/src/components/admin/EditableField.tsx
@@ -0,0 +1,50 @@
+import { Button, Stack, TextField, Typography } from "@mui/material";
+import React from "react";
+import styles from "../../styles/Admin.module.css";
+interface EditableFieldProps {
+  title: string;
+  value: string;
+  onChange: (value: string) => void;
+  edited?: boolean;
+  onSave?: () => void;
+  onReset?: () => void;
+}
+
+export default function EditableField({
+  title,
+  value,
+  onChange,
+  edited = false,
+  onSave = () => {},
+  onReset = () => {},
+}: EditableFieldProps) {
+  return (
+    <Stack className={styles.EditableFieldContainer}>
+      <Typography
+        color={"white"}
+        fontSize={35}
+        fontFamily={"SourceSans"}
+        fontWeight={"bold"}
+      >
+        {title}
+      </Typography>
+      <TextField
+        focused
+        variant={"filled"}
+        value={value}
+        inputProps={{ style: { color: "#ffffff" } }}
+        onChange={(e) => onChange(e.target.value)}
+      />
+      {edited && (
+        <Stack direction={"row"} marginTop={2}>
+          <Button variant="contained" color="success" onClick={onSave}>
+            Save
+          </Button>
+          <Button variant="contained" color="error" onClick={onReset}>
+            Discard
+          </Button>
+        </Stack>
+      )}
+    </Stack>
+  );
+}
