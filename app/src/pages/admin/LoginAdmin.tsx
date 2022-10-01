6c9306ed94d53c7629bc76079dd862da2f264ca7
Daniel Rygaard <daniel.rygaard@weknowit.se>
2022-10-01 19:43:35 +0200

    Created amin page

---
+++ app/src/pages/admin/LoginAdmin.tsx
@@ -0,0 +1,6 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+export default function LoginAdmin() {
+  return <Box width={"100vw"} height={"100vw"} display={"flex"}></Box>;
+}
