21f96cf35cdf60bd770847188430920cb57b3bf2
Daniel Rygaard <daniel.rygaard@weknowit.se>
2022-10-01 19:37:42 +0200

    Added firebase config

---
+++ app/src/config/FirebaseConfig.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+export const firebaseConfig = {
+  apiKey: "AIzaSyAk1ktOGvYBO5N-cTVCR8068EouG_aMq6U",
+  authDomain: "personal-website-e5fa7.firebaseapp.com",
+  projectId: "personal-website-e5fa7",
+  storageBucket: "personal-website-e5fa7.appspot.com",
+  messagingSenderId: "411119889921",
+  appId: "1:411119889921:web:066f06dae5eeee0eb15a00",
+  measurementId: "G-2W6N49G8ZK",
+};
+
+const app = initializeApp(firebaseConfig);
+export const firestore = getFirestore(app);
